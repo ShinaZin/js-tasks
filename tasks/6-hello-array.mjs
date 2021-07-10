8a71a1c4374a67b614b49bd5dfb73578e98d4ee2
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-10 11:51:51 +0300

    TASK: Added arrays task

---
+++ tasks/6-hello-array.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { console } from '../libs/console.js';
+
+// Ввести: число count
+// Ввести: разные имена через <Enter> count раз
+// Вывести: Hello, имя для каждого введенного имени, а в конце "Hello to all"))
+
+// Использовать массив для сохранения имен (метод .push) и обычный цикл с счетчиком for (let i = 0; ...)
+// Темы: 
+// - https://learn.javascript.ru/array
+// - https://learn.javascript.ru/type-conversions 
+// - https://learn.javascript.ru/while-for
+
+async function main() {
+
+}
+
+main();
