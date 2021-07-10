aa4f339c3b28781e3fe904f6d7ac442b2d20c630
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-10 11:50:52 +0300

    TASK: Added function task

---
+++ tasks/4-hello-function.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { console } from '../libs/console.js';
+
+// Написать функцию hello(name), которая будет возвращать строчку Hello, name!
+// Ввести: имя
+// Вывести: Hello, имя!
+
+// Темы: https://learn.javascript.ru/function-basics
+// Дополнительно: 
+// - добавить второй параметр greeting, который будет подставляться вместо Hello, пример hello('Ann', 'Hola') => Hola, Ann!
+// - можно сделать второй параметр со значением по умолчанию = 'Hello'
+
+async function main() {
+
+}
+
+main();
