923cf7bdc9350e3df79fe75c36090253de20019b
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-10 11:50:18 +0300

    TASK: Added cycle task

---
+++ tasks/2-hello-count.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { console } from '../libs/console.js';
+
+// Ввести: имя
+// Ввести: число count
+// Вывести: имя count раз (циклом while)
+
+// Темы: https://learn.javascript.ru/while-for
+
+// Дополнительно: 
+// Использовать цикл for (let i = 0; ...) вместо while
+
+async function main() {
+  
+}
+
+main();
