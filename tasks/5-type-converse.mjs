1306a6245f33fe6d21ab5e8928a4f186e3c20102
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-10 11:51:23 +0300

    TASK: Added type conversion task

---
+++ tasks/5-type-converse.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { console } from '../libs/console.js';
+
+// Ввести: два числа через <Enter>
+// Вывести: максимальное из этих чисел
+
+// Темы: 
+// - https://learn.javascript.ru/ifelse
+// - https://learn.javascript.ru/type-conversions 
+// - https://learn.javascript.ru/function-basics
+
+// Дополнительно: 
+// - оформить в виде функции max(a, b)
+// - написать противоположную функцию min(a, b)
+
+async function main() {
+
+}
+
+main();
