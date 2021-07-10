3548b53b3df7ecf866452a3c6f784dc349beace0
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-10 11:49:58 +0300

    TASK: Added simple IO tasks

---
+++ tasks/0-echo.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { console } from '../libs/console.js';
+
+// для запуска node ./tasks/0-echo
+// Выводит то, что ввел пользователь
+
+async function main() {
+  const input = await console.prompt('Enter something: ');
+  console.log(input);
+}
+
+main();
