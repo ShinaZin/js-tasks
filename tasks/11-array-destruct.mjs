793de6f003a28d2593dde83c9e57bd25062d9686
Pavel Zinovev <ShinaZin@mail.ru>
2021-07-31 16:03:34 +0300

    TASK: array-destruction added

---
+++ tasks/11-array-destruct.mjs
@@ -0,0 +1,14 @@
+// Написать функцию firstAndSecond, которая вернет новый массив, содержащий первый и второй элемент массива
+// Написать функцию secondAndOther, которая вернет объект, содержащий два поля:
+// second: второй элемент массива
+// other: последующие элементы
+
+// Дополнительно: использовать синтаксис деструктуризации
+
+function main() {
+  const numbers = [2, 1, 9, 3, -2];
+  console.log(firstAndSecond(numbers));
+  console.log(secondAndOther(numbers));
+}
+
+main();
