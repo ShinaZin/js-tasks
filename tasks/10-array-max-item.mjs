cce7470fe3cccfc30b71a1907c376a10cbc05ce1
Pavel Zinovev <ShinaZin@mail.ru>
2021-11-06 19:13:46 +0300

    TASK: find-max solved

--- tasks/10-array-max-item.mjs
+++ tasks/10-array-max-item.mjs
@@ -12,11 +12,34 @@ import { console } from '../libs/console.js';
 // - Циклы
 // - Операции сравнения (<, >, ..)
 
-// Дополнтельно
+// Дополнительно
 // Написать функцию findMin и вывести минимальный элемент массива
 
 async function main() {
-
+  const arrayOfRandomNumbers = [];
+  for (let i = 0; i < 10; i++){
+    arrayOfRandomNumbers.push(random(-100, 100));
+  } 
+  
+  console.log(arrayOfRandomNumbers);
+  console.log(findMax(arrayOfRandomNumbers)); 
 }
 
 main();
+
+
+function findMax(arr) {
+  let first = arr[0];
+  let second;
+  for (let i = 0; i < arr.length; i++){
+    second = arr[i];
+    if (second > first) { 
+      first = second;
+    }
+  }
+  return first;
+}
+
+function random(a, b) {
+  return Math.floor(a + Math.random() * (b - a));
+}
